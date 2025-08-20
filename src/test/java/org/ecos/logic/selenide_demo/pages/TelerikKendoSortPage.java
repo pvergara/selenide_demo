818ddcccb52b80a16f8d1ba6d8464ba62e38db35
pvergara <ecos.logic.org@gmail.com>
2025-08-20 22:30:25 +0200

    10th iteration (customizing):   * Now we work directly with Product on Step class hiding the mapping (for now) on its dependency (TelerikKendoSortPage)   * New method detail better the reason why the system need to sort the first time the Product name   * Removing the mapping method (the code lives happier on a class that implements the mapping interface ;-))

--- src/test/java/org/ecos/logic/selenide_demo/pages/TelerikKendoSortPage.java
+++ src/test/java/org/ecos/logic/selenide_demo/pages/TelerikKendoSortPage.java
@@ -1,8 +1,11 @@
 package org.ecos.logic.selenide_demo.pages;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
-import org.ecos.logic.selenide_demo.grid.control.Row;
-import org.ecos.logic.selenide_demo.grid.control.RowSingleImpl;
+import org.ecos.logic.selenide_demo.grid.control.row.RowAs;
+import org.ecos.logic.selenide_demo.grid.control.row.RowSingleImpl;
+import org.ecos.logic.selenide_demo.grid.kendo.sort.Mapper;
+import org.ecos.logic.selenide_demo.grid.kendo.sort.Product;
 import org.springframework.boot.test.context.TestComponent;
 
 import java.util.ArrayList;
@@ -12,34 +15,45 @@ import java.util.Objects;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static java.lang.String.format;
-import static org.ecos.logic.selenide_demo.grid.control.RowPlace.*;
+import static org.ecos.logic.selenide_demo.grid.control.row.RowPlace.First;
+import static org.ecos.logic.selenide_demo.grid.control.row.RowPlace.Next;
 
 @TestComponent
 public class TelerikKendoSortPage {
+
+    public static final String TELERIK_SORT_GRID_ROW_STRING_PARTIAL_SELECTOR = "tr[data-grid-row-index='%s'] td[aria-colindex='%d']";
+    public static final String ROW_COUNTER_ATTRIBUTE = "aria-rowcount";
+    public static final int NUMBER_OF_COLUMNS = 3;
+    private final Mapper mapper = new Mapper();
+
+    public final SelenideElement telerik_sort_grid_row_counter = Selenide.$(".k-grid.k-grid-md.k-grid-virtual > .k-grid-aria-root");
 
     public void openTelerikSortingPage() {
         open("https://demos.telerik.com/kendo-react-ui/grid/sorting/func?theme=default-main-dark");
     }
 
-    public List<Row> getAllGridRows() {
-        List<Row> result = new ArrayList<>();
-        String selectorKey = "tr[data-grid-row-index='%s'] td[aria-colindex='%d']";
-        int rowCount = Integer.parseInt(Objects.requireNonNull($(".k-grid.k-grid-md.k-grid-virtual > .k-grid-aria-root").getAttribute("aria-rowcount")));
+    public List<Product> getAllGridRows() {
+        List<RowAs<Product>> result = new ArrayList<>();
+        String selectorKey = TELERIK_SORT_GRID_ROW_STRING_PARTIAL_SELECTOR;
+        int rowCount = Integer.parseInt(Objects.requireNonNull(this.telerik_sort_grid_row_counter.getAttribute(ROW_COUNTER_ATTRIBUTE)));
+
         for (int i = 0; i < rowCount; i++) {
-            Row row = new RowSingleImpl(3);
+            RowAs<Product> row = new RowSingleImpl<>(NUMBER_OF_COLUMNS);
 
             SelenideElement firstColumn = $(format(selectorKey, i, 1));
             SelenideElement secondColumn = $(format(selectorKey, i, 2));
             SelenideElement thirdColumn = $(format(selectorKey, i, 3));
 
             firstColumn.click();
-            row.setThe(First).elementAs(Integer.valueOf(firstColumn.getText()));
-            row.setThe(Second).elementAs(secondColumn.getText());
-            row.setThe(Last).elementAs(Float.valueOf(thirdColumn.getText()));
+
+            row.setThe(First).elementAs(() -> Integer.valueOf(firstColumn.getText()));
+            row.setThe(Next).elementAs(secondColumn::getText);
+            row.setThe(Next).elementAs(()-> Float.valueOf(thirdColumn.getText()));
 
             result.add(row);
         }
-        return result;
+
+        return result.stream().map(item->item.getAllElementsAs(this.mapper)).toList();
     }
 
 
--- src/test/java/org/ecos/logic/selenide_demo/steps/TelerikKendoSortSeps.java
+++ src/test/java/org/ecos/logic/selenide_demo/steps/TelerikKendoSortSeps.java
@@ -3,8 +3,6 @@ package org.ecos.logic.selenide_demo.steps;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.ecos.logic.selenide_demo.grid.control.Row;
-import org.ecos.logic.selenide_demo.grid.control.RowPlace;
 import org.ecos.logic.selenide_demo.grid.kendo.sort.Product;
 import org.ecos.logic.selenide_demo.pages.TelerikKendoSortPage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,13 +19,13 @@ import static org.hamcrest.Matchers.is;
 public class TelerikKendoSortSeps {
     @Autowired
     private TelerikKendoSortPage page;
-    private List<Row> data;
+    private List<Product> data;
 
-    private void setData(List<Row> data) {
+    private void setData(List<Product> data) {
         this.data = data;
     }
 
-    private List<Row> getData() {
+    private List<Product> getData() {
         return data;
     }
 
@@ -38,8 +36,14 @@ public class TelerikKendoSortSeps {
 
     @Given("The rows are unsorted")
     public void theRowsAreUnsorted() {
-        theUserSortsTheTelerikKendoGridByColumn("Product Name");
+        //By default, the grid is sorted by Product Name Desc (the "reverse" order) so we sort it by Product Name
+        // to have the data grid unsorted
+        this.letTheGridUnsorted();
         this.setData(this.page.getAllGridRows());
+    }
+
+    private void letTheGridUnsorted() {
+        theUserSortsTheTelerikKendoGridByColumn("Product Name");
     }
 
     @When("The user sorts the Telerik Kendo Grid by {string} column")
@@ -57,20 +61,14 @@ public class TelerikKendoSortSeps {
 
         if (columnName.equals("Product Name")) {
             //I use the sublist in this case because of a strange way of sorting the data on the "Telerik side" with product names like "Sirop d'érable"
-            List<Product> listPreviousProductNames = this.getData().stream().map(this::mapElementToProduct).sorted((item1,item2)->collator.compare(item1.ProductName(),item2.ProductName())).toList().
+            List<Product> listPreviousProductNames = this.getData().stream().
+                    sorted((item1, item2)->collator.compare(item1.ProductName(),item2.ProductName())).toList().
                     subList(0, 50);
 
-            List<Product> listOfSortedProductNames = this.page.getAllGridRows().stream().map(this::mapElementToProduct).toList().
-                    subList(0, 50);
+            List<Product> listOfSortedProductNames = this.page.getAllGridRows().stream().
+                    toList().subList(0, 50);
 
             assertThat(listPreviousProductNames, is(equalTo(listOfSortedProductNames)));
         }
-    }
-
-    private Product mapElementToProduct(Row item) {
-        return new Product(
-            item.getTheElementAsInteger(RowPlace.First),
-            item.getTheElementAsString(RowPlace.Second),
-            item.getTheElementAsFloat(RowPlace.Third));
     }
 }
